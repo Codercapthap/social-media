99e27b73a814b9350be57e7535f9467b0ca87a51
Lillya <kidkaito1412.1@gmail.com>
2023-07-19 23:50:37 +0700

    change path to firebase key

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,7 +1,7 @@
 import { createRequire } from "module"; // Bring in the ability to create the 'require' method
 const require = createRequire(import.meta.url); // construct the require method
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("./etc/secrets/serviceAccountKey.json");
 
 export const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
