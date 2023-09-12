3cfe94512d10574d2ba34ea0aec115338f4d55c9
Lillya <kidkaito1412.1@gmail.com>
2023-09-13 02:24:05 +0700

    FIX: fix connecting database handle
    The database connection always print success even failure, this commit
    will fix it

--- server/database/db.js
+++ server/database/db.js
@@ -5,13 +5,11 @@ dotenv.config();
 // connect database
 export default function connectDB() {
   try {
-    mongoose.connect(
-      process.env.MONGO_URL,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      () => {
-        console.log("connected to mongodb");
-      }
-    );
+    mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connect successfully");
   } catch (err) {
     console.log(err);
   }
